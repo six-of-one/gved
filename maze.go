d82904ad348170a7ff45bf751a02df34fe5340f0
six-of-one <quakec@proton.me>
2025-11-27 01:42:30 -0500

    math transform mirror in y, have maze edge copy issues

--- maze.go
+++ maze.go
@@ -73,7 +73,9 @@ func domaze(arg string) {
 		for ty := 0; ty <= lasty; ty++ {
 			for tx := 0; tx <= lastx; tx++ {
 // mirror x
-				xform[xy{lastx - tx, ty}] = maze.data[xy{tx, ty}]
+//				xform[xy{lastx - tx, ty}] = maze.data[xy{tx, ty}]
+// mirror y: flip
+				xform[xy{tx, lasty - ty}] = maze.data[xy{tx, ty}]
 			}
 		}
 // copy back
