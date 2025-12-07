f3ba89bc366dc4a482ede2a4361b863ed9ebef53
six-of-one <quakec@proton.me>
2025-12-07 00:15:09 -0500

    test to see if draw frame being called too much - still same fail

--- maze.go
+++ maze.go
@@ -282,6 +282,10 @@ func domaze(arg string) {
 			mx := (gotilengine.CInt) (int (0))
 			my := (gotilengine.CInt) (int (0))
 			gtk = 0
+			if gotilengine.TLN_ProcessWindow() != 0 {
+				gotilengine.TLN_DrawFrame(0)
+			}
+
 			for gotilengine.TLN_ProcessWindow() != 0 && nokp {
 				tewtil := (gotilengine.CString) (C.CString(fmt.Sprintf("Maze: %d @ [%d, %d] - ly: %d, ½ frames %d, <-%s",mazeNum + 1,mx,my,ly,gtk,suser)))
 				gotilengine.TLN_SetWindowTitle(tewtil)
@@ -297,15 +301,23 @@ func domaze(arg string) {
 						case 1:
 							my--
 							time.Sleep(10 * time.Millisecond)
+							gotilengine.TLN_SetLayerPosition(ly,mx,my)
+							gotilengine.TLN_DrawFrame(0)
 						case 2:
 							my++
 							time.Sleep(10 * time.Millisecond)
+							gotilengine.TLN_SetLayerPosition(ly,mx,my)
+							gotilengine.TLN_DrawFrame(0)
 						case 3:
 							mx--
 							time.Sleep(10 * time.Millisecond)
+							gotilengine.TLN_SetLayerPosition(ly,mx,my)
+							gotilengine.TLN_DrawFrame(0)
 						case 4:
 							mx++
 							time.Sleep(10 * time.Millisecond)
+							gotilengine.TLN_SetLayerPosition(ly,mx,my)
+							gotilengine.TLN_DrawFrame(0)
 						case 5:
 							anum = mazeNum
 							suser = "maze -1"
@@ -322,20 +334,20 @@ func domaze(arg string) {
 								suser = "disab CRT"
 								crt = false
 								time.Sleep(250 * time.Millisecond)
+								gotilengine.TLN_DrawFrame(0)
 							} else {
 								gotilengine.TLN_ConfigCRTEffect(1,0)
 								suser = "enabl CRT"
 								crt = true
 								time.Sleep(250 * time.Millisecond)
+								gotilengine.TLN_DrawFrame(0)
 							}
 						default:
 							nokp = false
 						}
 					}}
 				gtk++
-				gotilengine.TLN_SetLayerPosition(ly,mx,my)
-//				time.Sleep(6 * time.Millisecond)
-				gotilengine.TLN_DrawFrame(0)
+				time.Sleep(6 * time.Millisecond)
 			}
 			if gotilengine.TLN_ProcessWindow() == 0 {
 				fmt.Printf("Exited window...\n")
