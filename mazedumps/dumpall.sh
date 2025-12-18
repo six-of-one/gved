0281641d21b666a5836f8b2d393cc086311cc99e
Six <quakec@proton.me>
2025-12-17 23:39:19 -0500

    a further update after i retooled dumpalls to update g2 renders and all dump txt files

--- mazedumps/dumpall.sh
+++ mazedumps/dumpall.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
-for i in $(seq -f "%03g" 1 117)
-do
-    ../gved -v -o maze$i.png maze$i > maze$i.txt
-done
+#!/bin/bash
+
+if [[ -f "./gved" ]]; then
+
+for i in $(seq -f "%03g" 1 117);do ./gved -g2 -v -o mazedumps/maze$i.png maze$i > mazedumps/maze$i.txt;done
+
+else
+
+echo "run $0 from go build . dir"
+
+fi
