98ad5710dbd97f2e1a5055115f1a5af31e55fb0e
Six <quakec@proton.me>
2025-12-17 23:20:19 -0500

    mazedumps g2 upd: pyramid gens now have monster type: D, G, L, S

--- mazedumps/dumpall.sh
+++ mazedumps/dumpall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in $(seq -f "%03g" 0 116)
+for i in $(seq -f "%03g" 1 117)
 do
-    ../gex -v -o maze$i.png maze$i > maze$i.txt
+    ../gved -v -o maze$i.png maze$i > maze$i.txt
 done
