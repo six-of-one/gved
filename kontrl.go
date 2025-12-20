4ab3f2b275e5102730b2f277f409914164b20fb4
Six <quakec@proton.me>
2025-12-20 02:06:30 -0500

    removed

--- kontrl.go
+++ kontrl.go
@@ -12,7 +12,6 @@ import (
     "fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/dialog"
     "fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/widget"
 	"fyne.io/fyne/v2/driver/desktop"
 )
 
@@ -384,23 +383,12 @@ func aw_init() {
 	}
 }
 
-type tappableIcon struct {
-	widget.Icon
-}
-
-func newTappableIcon(res *canvas.Raster) *tappableIcon {
-	icon := &tappableIcon{}
-	icon.ExtendBaseWidget(icon)
-	icon.SetResource(res)
-
-	return icon
-}
 // update contents
 
 func upwin(simg *image.NRGBA) {
 
 	bimg := canvas.NewRasterFromImage(simg)
-	w.Canvas().SetContent(newTappableIcon(bimg))
+	w.Canvas().SetContent(bimg)
 	geow := int(math.Max(560,opts.Geow))	// 556 is min, maze doesnt seem to fit or shrink smaller
 	geoh := int(math.Max(594,opts.Geoh))	// 594 min
 	w.Resize(fyne.NewSize(float32(geow), float32(geoh)))
