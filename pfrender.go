03b7e56b0e0ed7d3f16e7340c382b2be3bfa2154
Six <quakec@proton.me>
2026-01-24 22:11:33 -0500

    commentary cleanup so sections 'collapse' in editor

--- pfrender.go
+++ pfrender.go
@@ -22,6 +22,7 @@ var g2mask [256]int
 //
 // If we're not wrapping in a direction, we need to duplicate the left and
 // top walls, so that the maze will be enclosed.
+// six - note: this is only for appearance of viewer...
 func copyedges(maze *Maze) {
 	for i := 0; i <= 32; i++ {
 		if (maze.flags & LFLAG4_WRAP_H) == 0 {
@@ -222,7 +223,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 					if (nothing & NOWALL) == 0 {
 						stamp = wallGetStamp(maze.wallpattern, adj, maze.wallcolor)
 				}
-// test of some items not place in mazes
+ // test of some items not place in mazes
 				case MAZEOBJ_TILE_FLOOR:
 					if opts.SP {
 						ts := rand.Intn(470)
@@ -249,7 +250,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 					if (nothing & nwt) == 0 {
 						stamp = wallGetStamp(maze.wallpattern, adj, maze.wallcolor)
 				}
-// test of some items not place in mazes - place in empty floor tile @random
+ // test of some items not place in mazes - place in empty floor tile @random
 				case MAZEOBJ_TILE_FLOOR:
 					if opts.SP {
 						ts := rand.Intn(470)
@@ -269,6 +270,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 	}
 
 	opr := 3				// 3 sets of special potions
+// main maze decode loop - op over all maze cells
 	for y := 0; y <= lasty; y++ {
 		for x := 0; x <= lastx; x++ {
 			var stamp *Stamp
@@ -284,7 +286,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 				}
 // g2 decodes
 			if G2 {
-// hack for score table map display of: gold bag after treasure box, special potions
+ // hack for score table map display of: gold bag after treasure box, special potions
 	if x < (lastx - 1) && mazenum == 103 {	// dont hit past end of array & only do on score table maze
 		ts := maze.data[xy{x, y}]
 		tt := maze.data[xy{x+1, y}]
@@ -312,8 +314,8 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 	}
 			// We should do better
 			switch whatis(maze, x, y) {
-// specials are jammed in somewhere in G2 code, we just do this
-// specials added after convert to se id'ed them on maze 115, score table block
+ // specials are jammed in somewhere in G2 code, we just do this
+ // specials added after convert to se id'ed them on maze 115, score table block
 			case 70:
 				stamp = itemGetStamp("speedpotion")
 			case 71:
@@ -509,7 +511,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 				if nothing & NOEXP == 0 { stamp = ffGetStamp(adj) }
 			case MAZEOBJ_TRANSPORTER:
 				stamp = itemGetStamp("tport")
-// testing special potions
+ // testing special potions
 			case MAZEOBJ_HIDDENPOT:
 				if opts.SP {
 					ts := rand.Intn(6)
@@ -531,26 +533,28 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 			default:
 				if opts.Verbose && false { fmt.Printf("G² WARNING: Unhandled obj id 0x%02x\n", whatis(maze, x, y)) }
 			}
-// set mask flag in array
+ // set mask flag in array
 			if whatis(maze, x, y) > 0 && stamp != nil { g2mask[whatis(maze, x, y)] = stamp.mask }
 			}
+// end G2 decodes
+
 // g1 decodes
 			if G1 {
-// gen type op - put a letter on up left corner of every gen to indicate monsters
-//		brw G - grunts
-//		red D - demons
-//		yel L - lobbers
-//		pur S - sorceror
+ // gen type op - put a letter on up left corner of every gen to indicate monsters
+ //		brw G - grunts
+ //		red D - demons
+ //		yel L - lobbers
+ //		pur S - sorceror
 				gtop.Clear()
 				gtopl = ""// make sure g2 code (if it runs with g1) doesnt set extra dots on non walls
 				dots = 0
-// /fmt.Printf("g1 dec: %x -- ", whatis(maze, x, y))
+ // /fmt.Printf("g1 dec: %x -- ", whatis(maze, x, y))
 			switch whatis(maze, x, y) {
 
 			case G1OBJ_TILE_FLOOR:
 			// adj := checkwalladj3(maze, x, y) + rand.Intn(4)
 			// stamp = floorGetStamp(maze.floorpattern, adj, maze.floorcolor)
-// dont think g1 has stun tile
+ // dont think g1 has stun tile
 			case G1OBJ_TILE_STUN:
 				adj := checkwalladj3g1(maze, x, y) + rand.Intn(4)
 				stamp = floorGetStamp(maze.floorpattern, adj, maze.floorcolor)
@@ -559,7 +563,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 
 			case G1OBJ_TILE_TRAP1:
 				dots = 1
-//				fallthrough
+ //				fallthrough
 	/*
 			case G1OBJ_TILE_TRAP2:
 				if dots == 0 {
@@ -628,7 +632,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 				stamp = itemGetStamp("sorcerer")
 			case G1OBJ_MONST_DEATH:
 				stamp = itemGetStamp("death")
-// when death shows up on score board maze, hack theif into sample area
+ // when death shows up on score board maze, hack theif into sample area
 				if mazenum == 115 {
 					maze.data[xy{x, y + 3}] = G1OBJ_MONST_THIEF
 				}
@@ -642,7 +646,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 			case G1OBJ_GEN_GHOST3:
 				stamp = itemGetStamp("ghostgen3")
 
-// if a clear is done after, this SetRGB set bkg somehow
+ // if a clear is done after, this SetRGB set bkg somehow 
 			case G1OBJ_GEN_GRUNT1:
 				gtopl = "G"
 				if gtopcol { gtop.SetRGB(0.65, 0.3, 0.1) }
@@ -708,7 +712,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 				stamp = itemGetStamp("ipotion")
 			case G1OBJ_INVISIBL:
 				stamp = itemGetStamp("invis")
-// specials added after convert to se id'ed them on maze 115, score table block
+ // specials added after convert to se id'ed them on maze 115, score table block
 			case G1OBJ_X_SPEED:
 				stamp = itemGetStamp("speedpotion")
 			case G1OBJ_X_SHOTPW:
@@ -724,7 +728,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 
 			case G1OBJ_TRANSPORTER:
 				stamp = itemGetStamp("tportg1")
-// testing special potions - seperate from actual placed in maze above, this is the view tester option
+ // testing special potions - seperate from actual placed in maze above, this is the view tester option
 			case MAZEOBJ_HIDDENPOT:
 				if opts.SP {
 					ts := rand.Intn(6)
@@ -746,7 +750,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 			default:
 				if opts.Verbose && false { fmt.Printf("G¹ WARNING: Unhandled obj id 0x%02x\n", whatis(maze, x, y)) }
 			}
-// set mask flag in array
+ // set mask flag in array
 			if whatis(maze, x, y) > 0 && stamp != nil { g1mask[whatis(maze, x, y)] = stamp.mask }
 		}
 // Six: end G1 decode
@@ -772,6 +776,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 		}
 	}
 
+// set #T hider/includer flags that arent set above
 	g2mask[MAZEOBJ_WALL_REGULAR] = 2048
 	g2mask[MAZEOBJ_WALL_SECRET] = 1024
 	g2mask[MAZEOBJ_WALL_DESTRUCTABLE] = 1024
@@ -782,7 +787,7 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 	g2mask[MAZEOBJ_TILE_TRAP1] = 64
 	g2mask[MAZEOBJ_TILE_TRAP2] = 64
 	g2mask[MAZEOBJ_TILE_TRAP3] = 64
-//	g2mask[] =
+ //	g2mask[] =
 	g1mask[G1OBJ_WALL_REGULAR] = 2048
 	g1mask[G1OBJ_WALL_DESTRUCTABLE] = 1024
 	g1mask[G1OBJ_WALL_TRAP1] = 1024
@@ -795,30 +800,30 @@ func genpfimage(maze *Maze, mazenum int) *image.NRGBA {
 // - these can be used to make sprite sheets and the like
 
 /*******************************************
-TTT           ___  DDD                __
- T   i  L    E     D  d  u  u  m\/m  P  p
- T   i  L    E--   D  d  u  u  m  m  p__p
- T   i  LLL  E___  DDD    uu   m  m  p
+ TTT           ___  DDD                __
+  T   i  L    E     D  d  u  u  m\/m  P  p
+  T   i  L    E--   D  d  u  u  m  m  p__p
+  T   i  LLL  E___  DDD    uu   m  m  p
 
  *******************************************/
-// lock out with this - set to true to run dump
-// run decode for maze116
-// written to .p[0-31]/tl_%05d_%04X.png
-// where %d and %X are tile start addr
+ // lock out with this - set to true to run dump
+ // run decode for maze116
+ // written to .p[0-31]/tl_%05d_%04X.png
+ // where %d and %X are tile start addr
 
-// loops thru all palette nums for all valid tiles, that are known
-// atempts to correct for various 2x2, 3x3, 4x4 (dragon) and odd size doors
+ // loops thru all palette nums for all valid tiles, that are known
+ // atempts to correct for various 2x2, 3x3, 4x4 (dragon) and odd size doors
 if false {
-// counter for tiles - imprv - dont write dups
+ // counter for tiles - imprv - dont write dups
 	wcnt := 1
-// tb adder controls size of tile render, and mem skip to next tile
-// this could also control the render out 16x16, 24x24 or 32x32
+ // tb adder controls size of tile render, and mem skip to next tile
+ // this could also control the render out 16x16, 24x24 or 32x32
 	tbaddr := 9
 	var stamp *Stamp
 	stamp = itemGetStamp("ghost")
 	stamp.pnum = 0
 
-// 0000 - 1FFF dump
+ // 0000 - 1FFF dump
 
 	for stamp != nil && mazenum == 116 {
 
@@ -900,7 +905,7 @@ if false {
 			if i == 0x1cfb { i++ }
 		}
 		writile(stamp, 0x1d48, 4, 16 ,-0x800)
-// so doors are an absolute mess, with 2, 3, and 4 wide all mixed up 
+ // so doors are an absolute mess, with 2, 3, and 4 wide all mixed up 
 		stamp.width = 3
 		writile(stamp, 0x1d3c, 6, 24,-0x800)
 		writile(stamp, 0x1d42, 6, 24,-0x800)
@@ -913,7 +918,7 @@ if false {
 		writile(stamp, 0x1d52, 8, 32,-0x800)
 		writile(stamp, 0x1d60, 8, 32,-0x800)
 		writile(stamp, 0x1d6e, 8, 32,-0x800)
-// some of these doors are wrong
+ // some of these doors are wrong
 		for i := 0x1d7c; i < 0x1db3; i += tbaddr {
 			writile(stamp, i, 4, 16 ,-0x800)
 		}
@@ -933,17 +938,17 @@ if false {
 			writile(stamp, i, tbaddr, 24 ,-0x800)
 		}
 
-// every loop, increase palette # to next till end
+ // every loop, increase palette # to next till end
 		stamp.pnum++;
 
-// done, no further pallets
+ // done, no further pallets
 
 		if stamp.pnum == 32 {
 			stamp = nil
 		}
 	}
 
-// gauntlet 2 add ins not handled by 0000 - 1FFF dump
+ // gauntlet 2 add ins not handled by 0000 - 1FFF dump
 	pnum := 0
 
 	for pnum < 32 && mazenum == 116 {
@@ -964,7 +969,7 @@ if false {
 		stamp.pnum = pnum
 		writile(stamp, 0x25e4, 9, 24 ,0)
 
-// g2 temp powers
+ // g2 temp powers
 		stamp = itemGetStamp("transportability")
 		stamp.pnum = pnum
 		writile(stamp, 0x23fc, 4, 16 ,0)
@@ -980,8 +985,8 @@ if false {
 		stamp = itemGetStamp("supershot")
 		stamp.pnum = pnum
 		writile(stamp, 0x2788, 4, 16 ,0)
-// g1 powers
-// handled by loop now
+ // g1 powers
+ // handled by loop now
 
 		stamp = itemGetStamp("it")
 		tbaddr = 9
@@ -990,13 +995,13 @@ if false {
 
 			writile(stamp, i, tbaddr, 24 ,0)
 		}
-// pickles
+ // pickles
 		for i := 0x2300; i < 0x23fb; i += tbaddr {
 
 			writile(stamp, i, tbaddr, 24 ,0)
 		}
 		writile(stamp, 0x25db, tbaddr, 24 ,0)
-// ?
+ // ?
 		for i := 0x2400; i < 0x24fb; i += tbaddr {
 
 			writile(stamp, i, tbaddr, 24 ,0)
@@ -1005,7 +1010,7 @@ if false {
 
 			writile(stamp, i, tbaddr, 24 ,0)
 		}
-/* < x2000 handled by 1st loop now
+ /* < x2000 handled by 1st loop now
 		for i := 0x1fab; i < 0x1ffb; i += tbaddr {
 
 			writile(stamp, i, tbaddr, 24 ,0)
@@ -1014,9 +1019,9 @@ if false {
 
 			writile(stamp, i, tbaddr, 24 ,0)
 		}
-*/
+ */
 
-// dragon breath
+ // dragon breath
 		for i := 0x278c; i < 0x27f7; i += tbaddr {
 
 			writile(stamp, i, tbaddr, 24 ,0)
@@ -1044,7 +1049,7 @@ if false {
 			writile(stamp, i, tbaddr, 16 ,0)
 		}
 
-// only needed once - pnum 0, even tho this isnt the palette num on these
+ // only needed once - pnum 0, even tho this isnt the palette num on these
 		if pnum == 0 {
 			tbaddr = 4
 			stamp = itemGetStamp("exit4")
@@ -1065,11 +1070,11 @@ if false {
 			}
 		}
 
-// the single 8x8 tile set is locked out for normal tile writing ops
-// set true to run
-if false {
-// single tile, for all the issues
-// this is written to .8x8/c[0-31]/i%05d_%04X.png
+ // the single 8x8 tile set is locked out for normal tile writing ops
+ // set true to run
+ if false {
+ // single tile, for all the issues
+ // this is written to .8x8/c[0-31]/i%05d_%04X.png
 		stamp = itemGetStamp("ghost")
 		tbaddr = 1
 		stamp.pnum = pnum
@@ -1094,14 +1099,14 @@ if false {
 
 			writile(stamp, i, tbaddr, 8 ,0)
 		}
-// locked out
-}
-// the single 8x8 tile set end
-// this op is a beast to run and it takes a while
+ // locked out
+ }
+ // the single 8x8 tile set end
+ // this op is a beast to run and it takes a while
 
 		pnum++
 	}
-// Six - end of tile dumper
+ // Six - end of tile dumper
 }
 //
 // tile dumper ending
@@ -1116,9 +1121,9 @@ if opts.Verbose || opts.Se {
 	mz := mazenum + 1
 	wimg := blankimage(33, 33)
 	if opts.Se {
-// paste in sanctuary converter
+ // paste in sanctuary converter
 		if mz > 116 { mz = mz - 2 }	else {	// sanctuary does not have 115 as demo or 116 as score table
-// just a convention for se - these are already numbered in the system and not at 115 and 116
+ // just a convention for se - these are already numbered in the system and not at 115 and 116
 			if mz == 115 { mz = 0 }
 			if mz == 116 { mz = 127 }		// 1 past se end
 		}
@@ -1130,7 +1135,7 @@ if opts.Verbose || opts.Se {
 
 			if opts.Verbose { fmt.Printf(" %02d", maze.data[xy{x, y}]) }
 			if opts.Se {
-//				fmt.Printf("	SVRLOAD[1][3][%d] = \"0x%x\";\n", i, sanct_vrt[maze.data[xy{x, y}]])
+ //				fmt.Printf("	SVRLOAD[1][3][%d] = \"0x%x\";\n", i, sanct_vrt[maze.data[xy{x, y}]])
 				i++
 				var vr int
 				var hexc string
@@ -1159,7 +1164,7 @@ if opts.Verbose || opts.Se {
 		}
 		wrfile.Close()
 	}
-// Six end maze dumper
+ // Six end maze dumper
 }
 // maze dumper ending
 
