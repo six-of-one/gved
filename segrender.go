f69877a7377c1e3e32a133461d64410e49d93090
Six <quakec@proton.me>
2026-02-10 16:23:59 -0500

    floor build doesnt cover all the way from odd size (even powers of 2) source, maybe do a calc for overload

--- segrender.go
+++ segrender.go
@@ -568,7 +568,7 @@ fmt.Printf("xb,yb,xs,ys %d %d %d %d xba,yba %d %d, dimX,y %d %d\n",xb,yb,xs,ys,x
 		if Se_mflor >= 0 {
 			err, _, ptamp = itemGetPNG(Se_cflr[Se_mflor])
 			if err == nil {
-				flim = blankimage(opts.DimX*16, opts.DimY*16)
+				flim = blankimage((opts.DimX+10)*16, (opts.DimY+10)*16)
 				bnds := ptamp.Bounds()
 				iw, ih := bnds.Dx(), bnds.Dy()		  // in theory this image does not HAVE to be square anymore
 				for ty := 0; ty < (opts.DimY*16) ; ty=ty+ih {
