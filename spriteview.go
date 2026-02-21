df584e1555df756f67f24f47c95e89d2ee0f63c8
Six <quakec@proton.me>
2026-02-21 15:42:50 -0500

    adding a sprite viewer tab, to explor roms

---
+++ spriteview.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+)
+
+
+var sprview *fyne.Container
+
+func sprite_view() {
+
+	container.New(
+		layout.NewHBoxLayout(),
+	)
+}
