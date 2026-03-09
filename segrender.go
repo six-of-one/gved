3726fa9783959c382ddd85f059dc0a8576ff55a7
Six <quakec@proton.me>
2026-03-09 01:13:18 -0400

    more like

--- segrender.go
+++ segrender.go
@@ -1243,7 +1243,7 @@ if opts.Verbose { fmt.Printf("%03d ",sb) }
 	draw.Draw(rimg, wimg.Bounds(), wimg, image.ZP, draw.Over)
 	draw.Draw(rimg, mimg.Bounds(), mimg, image.ZP, draw.Over)
 
-if opts.Verbose {savetopng("seg.png", img)}
+if opts.Verbose {savetopng("seg.png", rimg)}
 
 	flordirt, walsdirt = 0,0		// whether rebuild layers or jsut animating mobs, reset these
 
